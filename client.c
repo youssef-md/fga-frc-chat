7f2dd959977ed11aff3e1e629506869e7462fb6b
Youssef Muhamad <me.youssefmuhamad@gmail.com>
2023-06-30 16:54:04 -0300

    feat: complete client code, sending the username and handling fds with select()
    Co-authored-by: Pedro Rodrigues <peddroprp@gmail.com>
    Co-authored-by: senaarth <senaarth@gmail.com>

--- client.c
+++ client.c
@@ -9,6 +9,42 @@
 
 #define BUFSIZE 1024
 
+void send_recv(int i, int sockfd) {
+	char send_buf[BUFSIZE];
+	char recv_buf[BUFSIZE];
+	int nbyte_recvd;
+
+	if (i == 0) {
+		fgets(send_buf, BUFSIZE, stdin);
+		send(sockfd, send_buf, strlen(send_buf), 0);
+	} else {
+		nbyte_recvd = recv(sockfd, recv_buf, BUFSIZE, 0);
+		recv_buf[nbyte_recvd] = '\0';
+		printf("%s\n", recv_buf);
+
+		fflush(stdout);
+	}
+}
+
+void connect_req(int *sockfd, struct sockaddr_in *server_addr, char *client_name) {
+	if ((*sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("Erro no socket()");
+		exit(1);
+	}
+
+	server_addr->sin_family = AF_INET;
+	server_addr->sin_port = htons(8001);
+	server_addr->sin_addr.s_addr = INADDR_ANY;
+	memset(server_addr->sin_zero, '\0', sizeof server_addr->sin_zero);
+
+	if (connect(*sockfd, (struct sockaddr *)server_addr, sizeof(struct sockaddr)) == -1) {
+		perror("Erro no connect()");
+		exit(1);
+	}
+
+	send(*sockfd, client_name, strlen(client_name) + 1, 0);
+}
+
 int main(int argc, char **argv) {
 	int sockfd, last_fd, i;
 	struct sockaddr_in server_addr;
